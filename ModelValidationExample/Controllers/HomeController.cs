d4792b7b03fa9126f3088c3a158914862cdb2b57
kaminatashin <katashin@gmail.com>
2024-12-10 23:25:47 +0330

    Add project files.

---
+++ ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+namespace ModelValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+          //  List<string> errorList=new List<string>();
+
+            ////////////////////////////////////////
+            ///
+
+
+
+            if (!ModelState.IsValid)
+            {
+               string errors =string.Join("\n" ,ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach(var error in value.Errors)
+                //    {
+                //        errorList.Add(error.ErrorMessage);
+                //    }
+                //}
+                //string errors=string.Join("\n",errorList);
+                return BadRequest(errors);
+            }
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationExample/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.Models
+{
+public class Person
+    {
+        [Required(ErrorMessage ="Person Name can't be emplty or null")]
+        public string? PersonName { get; set; }
+
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfrimPassword { get; set; }
+        public string? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person Object-person name:{PersonName} ,email:{Email},phone:{Phone},password:{Password},confirm Password :{ConfrimPassword},price:{Price}  ";
+        }
+    }
+
+}
